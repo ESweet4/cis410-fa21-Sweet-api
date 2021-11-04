87b493ef29bc596107273b7835029688e5b011bd
Eliana Sweet <esweet4@rams.colostate.edu>
2021-11-04 17:24:55 -0600

    11.2 11/4 Deployment Prep

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(5000, () => {
-  console.log("app is running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`app is running on port, ${PORT}`);
 });
 
 app.get("/hi", (req, res) => {
